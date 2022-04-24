cf8bd898ec58785beec1046e18301947a17e882e
Stanislaw1349 <alfstas@gmail.com>
2022-04-24 08:18:24 +0300

    add9

---
+++ HomeWorckS/src/hw_9_2/Director.java
@@ -0,0 +1,20 @@
+/*
+package hw_9_2;
+
+public class Director extends Manager {
+
+    public Director(String name, int baseSalary) {
+        super(name, baseSalary);
+    }
+
+    @Override
+    public int getSalary() {
+        if (numberOfSubordinates == 0){
+            return super.getSalary();
+        } else {
+            return getBaseSalary() * (numberOfSubordinates / 100 * 9);
+        }
+
+    }
+}
+*/
