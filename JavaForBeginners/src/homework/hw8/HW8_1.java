c0972c6ff1f8940d068c0c58a30fb9cc18c80787
Stanislaw1349 <alfstas@gmail.com>
2022-05-03 22:21:42 +0300

    add new task

---
+++ JavaForBeginners/src/homework/hw8/HW8_1.java
@@ -0,0 +1,8 @@
+package homework.hw8;
+
+public class HW8_1 {
+    //Создать объекты типа Integer, Double, String и присвоить им любые значения.
+    static Integer q1 = 20;
+    static Double d1 = 4.8;
+    static String str1 = "Java";
+}
