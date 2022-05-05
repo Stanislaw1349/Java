43b11d9e4ec0c9e1b725aea471a61f556ef15c47
Stanislaw1349 <alfstas@gmail.com>
2022-05-05 09:36:03 +0300

    add new task

---
+++ HomeWorckS/src/hw_5/HW5_1.java
@@ -0,0 +1,16 @@
+package hw_5;
+
+public class HW5_1 {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+    public int subtraction(int a, int b){
+        return a - b;
+    }
+    public int multiplication(int a, int b){
+        return a * b;
+    }
+    public double division(double a, double b){
+        return a / b;
+    }
+}
