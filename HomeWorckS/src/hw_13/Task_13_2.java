abf2f17ee859e8dfd236efa38c087eab11c825cc
Stanislaw1349 <alfstas@gmail.com>
2022-06-01 22:40:15 +0300

    refactor

--- HomeWorckS/src/hw_13/Task_13_2.java
+++ HomeWorckS/src/hw_13/Task_13_2.java
@@ -1,28 +1,21 @@
-/*
 package hw_13;
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 //Задача №2
 //Создать лист со значениями от 100 до 1000.
+
+
 public class Task_13_2 {
     public static void main(String[] args) {
 
-
-        int count = 100;
-        for (int i = 100; i < 1001; i++){
-            count++;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 100; i <= 1000; i++){
+            list.add(i);
         }
-        int[] arr = new int[count];
-
-        for (int i = 100, j = 100; i < arr.length; i++, j++){
-        arr[i] = j;
-        }
-
-        ArrayList<> arrayList = new ArrayList<>(Arrays.asList(arr));
-        System.out.println(list);
     }
 }
-*/
+
